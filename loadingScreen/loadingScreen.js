302ea9d7c9fcbd3ec6ed0e0929a14372216f17b2
Vlad Anghelus <anghelus.vlad30@gmail.com>
2021-04-22 17:04:20 +0300

    Loading screen

---
+++ loadingScreen/loadingScreen.js
@@ -0,0 +1,13 @@
+import './loadingScreen.css';
+
+function loadingScreen() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div class="loader"></div>
+      </header>
+    </div>
+  );
+}
+
+export default loadingScreen;
