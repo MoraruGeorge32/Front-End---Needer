54e13258257c8c0a43bc8a4b8e34c74b896d2462
RiZZeR <ramusteata@gmail.com>
2021-05-13 00:50:12 +0300

    stilizare cantitate cu Moraru George

--- needer-app/src/App-forum/alte_servicii_component/alte_servicii.js
+++ needer-app/src/App-forum/alte_servicii_component/alte_servicii.js
@@ -12,7 +12,15 @@ function hideCantitate() {
 }
 
 
+
 function AlteServicii() {
+    const submitHandler = e =>{
+        e.preventDefault()
+        fetch(`https://hooks.zapier.com/hooks/catch/10117216/byqhx9d/silent/`, {
+          method: 'POST',
+          //body: JSON.stringify({ email, comment }),
+        })
+      }
     React.useEffect(() => {
         document.getElementById("selectServiciu").addEventListener('onClick', hideCantitate());
         document.getElementById("selectProdus").addEventListener('onClick', showCantitate());
@@ -21,12 +29,13 @@ function AlteServicii() {
     return (
         <div class="wrapper" id="test">
             <div class="wrapper_form">
-                <form method="POST">
+                <form onSubmit={submitHandler}>
+                    {/*https://hooks.zapier.com/hooks/catch/10117216/byqhx9d/silent/*/ }
                     <div class="DescriereNevoie"><label>Descriere nevoie</label> </div> <br />
 
                     <label for="tip_nevoie">Alegeți tipul de nevoie</label><br />
-                    <input type="radio" id="selectServiciu" onClick={hideCantitate} name="tip_nevoie" value="Serviciu" /> Serviciu <br />
-                    <input type="radio" id="selectProdus" onClick={showCantitate} name="tip_nevoie" value="Produs" /> Produs
+                    <input type="radio" id="selectServiciu" onClick={hideCantitate} name="tip_nevoie" value="Serviciu" checked required/> Serviciu <br />
+                    <input type="radio" id="selectProdus" onClick={showCantitate} name="tip_nevoie" value="Produs" required/> Produs
                     <br />
                     <Tags />
                     <textarea id="descriere_text_box" name="textbox" />
@@ -34,17 +43,22 @@ function AlteServicii() {
                     {/*<label for="data_dorita">Data doriri finalizarii cererii {nevoie}</label>*/}
                     <div class="DescriereData"><span class="DataFinalizare">Data doririi finalizării cererii</span></div>
                     <br />
+                    <MyDatePicker />
                     <div class="Cantitate_produs" id="cantitateprodus">
-                        <label for="quantity">Cantitate (intre 1 si 100 de kilograme):</label>
-                        <input type="number" id="quantity" name="quantity" min="1" max="100"/><br />
+                        <div class="Cantitate_produs_1">
+                            <label for="quantity">Cantitate (intre 1 si 100 de kilograme):</label>
+                        </div>
+                        <input type="number" id="quantity" name="quantity" min="1" max="100"/>
+                        <br />
                     </div>
-                        <MyDatePicker />
                         {/*<input type="date" id="data_pentru_cerere" value={date_value_current} name="data_dorita"></input>*/}
-                        <Link to='/loading'>
+                        
                             <div class="alignRight">
-                                <input class="butonSubmit" type="submit">
-                                </input></div>
-                        </Link>
+                                <Link to='/loading'>
+                                    <input class="butonSubmit" type="submit"></input>
+                                </Link>
+                            </div>
+                        
                 </form>
             </div>
             </div>
